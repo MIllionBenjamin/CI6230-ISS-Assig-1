befdcfe848d583a4ac32fbdb302bc0b0a9048185
MillionBenjain <617572667@qq.com>
2021-10-17 17:10:07 +0800

    Create db_initialization.sql

---
+++ db_initialization.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE user_test;
+
+use user_test;
+
+CREATE TABLE user (
+    username VARCHAR(20) NOT NULL, 
+    email_address VARCHAR(30) NOT NULL,
+    home_address varchar(50) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+insert into user
+(username, email_address, home_address)
+values
+('Alice', 'alice@ntu.edu.sg', 'Hall 1 201'),
+('Bob', 'bob@ntu.edu.sg', 'Graduate Hall 2 301'),
+('Chris', 'chris@ntu.edu.sg', 'Tan Lark Sye Walk 44');
+
+CREATE USER 'read_only'@'localhost' IDENTIFIED BY 'readonly1';
+GRANT SELECT ON *.* TO 'read_only'@'localhost';
