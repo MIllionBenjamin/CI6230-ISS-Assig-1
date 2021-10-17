2cf94a6993410a7a3e8847bbc33d4d9a330c019a
MillionBenjain <617572667@qq.com>
2021-10-17 17:09:34 +0800

    20211017

---
+++ unsafe/unsafe_sql_api.py
@@ -0,0 +1,31 @@
+import pymysql
+import logging
+
+class UserDBManager:
+    def __init__(self, db_config: dict):
+        self.db_connection = pymysql.connect(host = db_config["host"], 
+                                             port = db_config["port"], 
+                                             user = db_config["user"], 
+                                             password = db_config["password"], 
+                                             database = db_config["database"])
+        self.cursor = self.db_connection.cursor()
+        self.name = "unsafe_UserDBManager-"+ db_config["user"] + "-" + db_config["host"] + "-" + db_config["database"]
+        print(self.name)
+        logging.basicConfig(filename = self.name + ".log")
+        self.logger = logging.getLogger()
+        print("Log File:", self.name + ".log")
+    
+    def __del__(self):
+        self.db_connection.commit()
+        self.db_connection.close()
+
+    
+    def get_user_info_with_email_address(self, email_address: str):
+        try:
+            self.cursor.execute("select * from user where email_address = '%s'" % (email_address))
+            result = self.cursor.fetchall()
+            return result
+        except Exception as e:
+            self.logger.error("error in get_todo_task: " + str(e))
+            return []
+            
---
+++ unsafe/using_unsafe_sql_api_RESTful.py
@@ -0,0 +1,31 @@
+# request with injection:
+# http://127.0.0.1:8383/user?email=alice@ntu.edu.sg%27%20or%20%271%27=%271
+
+DB_CONFIG = {
+    "host": "127.0.0.1", 
+    "port": 3306, 
+    "user": "read_only", 
+    "password": "readonly1", 
+    "database": "user_test"
+}
+
+from unsafe_sql_api import UserDBManager
+from flask import Flask, abort, request, jsonify
+from flask.views import MethodView
+
+user_manager = UserDBManager(db_config = DB_CONFIG)
+
+app = Flask(__name__)
+
+
+
+class User(MethodView):
+    def get(self):
+        email_address = str(request.args["email"])
+        return jsonify(user_manager.get_user_info_with_email_address(email_address))
+app.add_url_rule('/user', view_func = User.as_view(name='user'))
+
+
+
+if __name__ == '__main__':
+    app.run(host = "0.0.0.0", port = 8383)
